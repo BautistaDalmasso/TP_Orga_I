6d260901efda81f143cf9c03b52047b68d800308
natag712 <116913724+natag712@users.noreply.github.com>
2022-11-06 19:04:36 -0300

    Update tp.asm

--- tp.asm
+++ tp.asm
@@ -194,61 +194,61 @@
 		bx lr
 		.fnend
 
- /* Guarda la coordenada y luego obtiene el valor de la misma
-        input= -
-        output= r1 <- valor de la coordenada */
-        obtenerCoordenada:
-        .fnstart
-        push { r0, r2, r3, r4, r5, r6, r7, lr }
+	 /* Guarda la coordenada y luego obtiene el valor de la misma
+		input= -
+		output= r1 <- valor de la coordenada */
+		obtenerCoordenada:
+		.fnstart
+		push { r0, r2, r3, r4, r5, r6, r7, lr }
 
-        //guardo la direccion de la coordenada ascii en r1
-        mov r7,#3
-        mov r0,#0
-        mov r2,#1
-        ldr r1,=coordenada
-        swi 0
+		//guardo la direccion de la coordenada ascii en r1
+		mov r7,#3
+		mov r0,#0
+		mov r2,#1
+		ldr r1,=coordenada
+		swi 0
 
-        //rescato el valor (en ascii), y obtengo su valor
-        ldrb r1,[r1]
-        mov r0, #0x30
-        sub r1,r0
-        pop { r0, r2, r3, r4, r5, r6, r7, lr }
-        bx lr
-        .fnend
+		//rescato el valor (en ascii), y obtengo su valor
+		ldrb r1,[r1]
+		mov r0, #0x30
+		sub r1,r0
+		pop { r0, r2, r3, r4, r5, r6, r7, lr }
+		bx lr
+		.fnend
 
-        /* Solicita coordenada (x,y)  y obtiene el valor de cada coordenada
-        input= -
-        output= en r2 coordenada x , en r3 coordenada y
-        */
-        pedirCoordenadas:
-        .fnstart
-        push {r0, r1, r4, r7, lr}
-        //Ingresamos mensaje para x
-        mov r7,#4
-        mov r0,#1
-        mov r2,#37
-        ldr r1,=mensaje_x
-        swi 0
+		/* Solicita coordenada (x,y)  y obtiene el valor de cada coordenada
+		input= -
+		output= en r2 coordenada x , en r3 coordenada y
+		*/
+		pedirCoordenadas:
+		.fnstart
+		push {r0, r1, r4, r7, lr}
+		//Ingresamos mensaje para x
+		mov r7,#4
+		mov r0,#1
+		mov r2,#37
+		ldr r1,=mensaje_x
+		swi 0
 
-        bl obtenerCoordenada
+		bl obtenerCoordenada
 
-	 mov r6,r1 @en r6 queda el valor que tiene que ir en  r2
+		 mov r6,r1 @en r6 queda el valor que tiene que ir en  r2
 
 
-        //Ingresamos mensaje para y
+		//Ingresamos mensaje para y
 
-        mov r7,#4
-        mov r0,#1
-        mov r2,#37
-        ldr r1,=mensaje_y
-        swi 0
+		mov r7,#4
+		mov r0,#1
+		mov r2,#37
+		ldr r1,=mensaje_y
+		swi 0
 
-        bl obtenerCoordenada
-        mov r5,r1 @en r5 queda el valor de r3
+		bl obtenerCoordenada
+		mov r5,r1 @en r5 queda el valor de r3
 
-        pop {r0, r1, r4, r7, lr}
-        bx lr
-        .fnend
+		pop {r0, r1, r4, r7, lr}
+		bx lr
+		.fnend
 
 
 	.global main
